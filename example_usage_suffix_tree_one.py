747fc3dc0cabdb5e575edee0cd16241a317f6a5e
Shushuda <shushuda.dev@gmail.com>
2020-06-06 18:24:22 +0200

    Last fixes and stuff

--- example_usage_suffix_tree_one.py
+++ example_usage_suffix_tree_one.py
@@ -7,9 +7,11 @@ import suffix_tree_my_imp
 # w tym przykladzie k == 5
 Z = {"iksde", "de iks", "iks de", "de", "iks", "ksde"}
 text = "#".join(Z) + "$"
+substring = "sde"
 
 print("String jako calosc:", text)
 print("String jako zbior:", Z)
+print("Poszukiwany substring (jedno wystepowanie):", substring)
 
 print("-------------------")
 print("Generowanie drzewa z tekstu jako calosc:")
@@ -19,12 +21,16 @@ substring_count_1, substrings_1 = tree_1.count_substrings_in_strings()
 print(f"Lancuchow bedacych podlancuchem innego lancucha zbioru Z "
       f"jest: {substring_count_1}")
 print(f"Znalezione podlancuchy: {substrings_1}")
+print(f"Czy substring '{substring}' jest podlancuchem lancucha Z?")
+print(tree_1.find_one_substring(substring))
 
 print("-------------------")
 print("Generowanie drzewa z tekstu jako zbior:")
-tree2 = suffix_tree_my_imp.GenSuffixTree(Z, joined_string=False)
-tree2.build_suffix_tree()
-substring_count_2, substrings_2 = tree2.count_substrings_in_strings()
+tree_2 = suffix_tree_my_imp.GenSuffixTree(Z, joined_string=False)
+tree_2.build_suffix_tree()
+substring_count_2, substrings_2 = tree_2.count_substrings_in_strings()
 print(f"Lancuchow bedacych podlancuchem innego lancucha zbioru Z "
       f"jest: {substring_count_2}")
 print(f"Znalezione podlancuchy: {substrings_2}")
+print(f"Czy substring '{substring}' jest podlancuchem zbioru lancuchow Z?")
+print(tree_2.find_one_substring(substring))
